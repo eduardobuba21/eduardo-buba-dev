1538517d7e6c91c55186e28dca675dc8e71f13ad
eduardo buba <eduardobuba21@gmail.com>
2023-04-12 18:51:48 -0300

    feat: profile image init

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Image from 'next/image';
 import { mq } from '@/utils/theme';
 import styled from 'styled-components';
 
@@ -10,9 +11,23 @@ export default function About() {
     <Container>
       <PostContent>
         <PostContainer>
-          <div>
-            <h1>Sobre</h1>
-          </div>
+          <PostMain selectionColor="#b248d5">
+            <GradientTitle primaryColor="#b248d5" secondaryColor="#9146e7">
+              Sobre.
+            </GradientTitle>
+
+            <ImgBase>
+              <ProfileImg
+                alt="Eduardo"
+                src="/profile.jpg"
+                height="250"
+                width="250"
+                placeholder="blur"
+                blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAAECAIAAAAmkwkpAAAACXBIWXMAABYlAAAWJQFJUiTwAAAAP0lEQVQImQE0AMv/AFBQUJKSkqmpqaOjowCurq7v7+/Jycm5ubkA////jIyMn5+fg4ODADAwMD09PWlpaQAAAApRGnEHblMWAAAAAElFTkSuQmCC"
+                priority
+              />
+            </ImgBase>
+          </PostMain>
         </PostContainer>
       </PostContent>
     </Container>
@@ -22,13 +37,51 @@ export default function About() {
 /* ---------------------------------------------------------------------- */
 
 const Container = styled.div`
-  align-items: center;
+  align-items: start;
   display: flex;
   flex: 1;
   margin: 0 auto;
   ${mq[1]} {
     width: 800px;
   }
+`;
+
+const navHeightMobile = '110px';
+const navHeightDesktop = '60px';
+
+const PostMain = styled.main.withConfig({
+  shouldForwardProp: (prop) => !['selectionColor'].includes(prop),
+})<{ selectionColor: string }>`
+  flex: 1 1;
+  overflow: hidden;
+  padding: ${navHeightMobile} 0;
+  ${mq[1]} {
+    padding: ${navHeightDesktop} 0;
+  }
+  & ::selection {
+    background: ${(p) => p.selectionColor};
+    color: #000000;
+    -webkit-text-fill-color: #000000;
+  }
+`;
+
+const GradientTitle = styled.h1.withConfig({
+  shouldForwardProp: (prop) =>
+    !['primaryColor', 'secondaryColor'].includes(prop),
+})<{ primaryColor: string; secondaryColor: string }>`
+  background-size: 100%;
+  background-clip: text;
+  -webkit-background-clip: text;
+  -moz-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  -moz-text-fill-color: transparent;
+  box-decoration-break: clone;
+
+  background-image: linear-gradient(
+    135deg,
+    ${(p) => p.primaryColor} 0%,
+    ${(p) => p.secondaryColor} 100%
+  );
 `;
 
 const PostContent = styled.div`
@@ -47,4 +100,48 @@ const PostContainer = styled.div`
   display: flex;
   align-items: center;
   gap: 40px;
+`;
+
+const ProfileImg = styled(Image)`
+  border-radius: var(--border-radius);
+`;
+
+const ImgBase = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 251px;
+  height: 251px;
+  padding: 25px 10px;
+  margin-left: 16px;
+  transform: translateZ(0);
+  border-radius: var(--border-radius);
+  overflow: hidden;
+  box-shadow: 0px 2px 8px -1px #0000001a;
+  &::before,
+  &::after {
+    content: '';
+    position: absolute;
+    z-index: -1;
+  }
+  /* conic gradient animation */
+  &::before {
+    animation: 6s rotate linear infinite;
+    width: 200%;
+    height: 200%;
+    background: var(--tile-border);
+  }
+  /* inner square */
+  &::after {
+    inset: 0;
+    padding: 1px;
+    border-radius: var(--border-radius);
+    background: linear-gradient(
+      to bottom right,
+      rgba(var(--tile-start-rgb), 1),
+      rgba(var(--tile-end-rgb), 1)
+    );
+    background-clip: content-box;
+  }
 `;
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import Image from 'next/image';
 import { mq } from '@/utils/theme';
 import styled from 'styled-components';
 import { motion, MotionContainer, mVariants } from '@/components/motion';
@@ -14,16 +13,6 @@ export default function Home() {
         <MotionContainer>
           <motion.div variants={mVariants.varFade().in}>
             <PostContainer>
-              {/* <ImgBase>
-              <ProfileImg
-                src="/profile.jpg"
-                height={250}
-                width={250}
-                alt="Imagem de perfil"
-                priority
-              />
-            </ImgBase> */}
-
               <div>
                 <motion.div variants={mVariants.varFade().inUp}>
                   <NameTypography>Eduardo Buba</NameTypography>
@@ -97,48 +86,4 @@ const PostContainer = styled.div`
 
 const NameTypography = styled.h1`
   filter: invert(0) drop-shadow(0 0 0.3rem #ffffff30);
-`;
-
-const ProfileImg = styled(Image)`
-  border-radius: var(--border-radius);
-`;
-
-const ImgBase = styled.div`
-  position: relative;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 251px;
-  height: 251px;
-  padding: 25px 10px;
-  margin-left: 16px;
-  transform: translateZ(0);
-  border-radius: var(--border-radius);
-  overflow: hidden;
-  box-shadow: 0px 2px 8px -1px #0000001a;
-  &::before,
-  &::after {
-    content: '';
-    position: absolute;
-    z-index: -1;
-  }
-  /* conic gradient animation */
-  &::before {
-    animation: 6s rotate linear infinite;
-    width: 200%;
-    height: 200%;
-    background: var(--tile-border);
-  }
-  /* inner square */
-  &::after {
-    inset: 0;
-    padding: 1px;
-    border-radius: var(--border-radius);
-    background: linear-gradient(
-      to bottom right,
-      rgba(var(--tile-start-rgb), 1),
-      rgba(var(--tile-end-rgb), 1)
-    );
-    background-clip: content-box;
-  }
 `;
